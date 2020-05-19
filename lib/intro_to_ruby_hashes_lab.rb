91749d009520c32faf0ada81ae8f6bb8848495fe
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-19 22:47:08 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,21 @@
 def new_hash
-  # return an empty hash
+  Hash.new 
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+ hash = {name: "Sanjeev", state: "Colorado"}
+ hash 
 end
 
 def pioneer
+  hash = {:name => "Grace Hopper"}
+  hash
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  hash = {:id => number}
+  hash 
   # return a hash with a key :id assigned to the provided number
 end
